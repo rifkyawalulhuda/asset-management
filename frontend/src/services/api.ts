186f18a376cd684ee74826b8ee460fa3067b357e
Rifky Awalul Huda <rifkyawalulhuda@gmail.com>
2026-07-14 20:17:42 +0700

    Update api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
   headers: { 'Content-Type': 'application/json' },
 })
 
 export default api
+
